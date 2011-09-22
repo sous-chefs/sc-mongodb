93868e5e6b25636e55d71e2f8ab5bd343f997458
Markus Korn <thekorn@gmx.de>
2011-09-22 15:17:11 +0200

    Do not restart mongod instances on every configure run, even if nothing changed (fixes issue 5)

--- mongodb/recipes/default.rb
+++ mongodb/recipes/default.rb
@@ -26,18 +26,21 @@ end
 needs_mongo_gem = (node.recipes.include?("mongodb::replicaset") or node.recipes.include?("mongodb::mongos"))
 
 if needs_mongo_gem
+  # install the mongo ruby gem at compile time to make it globally available
   gem_package 'mongo' do
     action :nothing
   end.run_action(:install)
   Gem.clear_paths
 end
 
-# configure default instance
-mongodb_instance "mongodb" do
-  mongodb_type "mongod"
-  port         node['mongodb']['port']
-  logpath      node['mongodb']['logpath']
-  dbpath       node['mongodb']['dbpath']
+if node.recipes.include?("mongodb::default") or node.recipes.include?("mongodb")
+  # configure default instance
+  mongodb_instance "mongodb" do
+    mongodb_type "mongod"
+    port         node['mongodb']['port']
+    logpath      node['mongodb']['logpath']
+    dbpath       node['mongodb']['dbpath']
+  end
 end
 
 ## Firewall configuration ##
