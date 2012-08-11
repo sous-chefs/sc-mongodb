293c185772a73e2e50b8a95cb55117782a3b5c89
Dan Crosta <dcrosta@late.am>
2012-08-11 10:36:05 -0400

    declare support for other platforms in metadata

--- metadata.rb
+++ metadata.rb
@@ -14,7 +14,7 @@ recipe "mongodb::replicaset", "Installs and configures a mongodb replicaset"
 depends "apt"
 depends "yum"
 
-%w{ ubuntu debian freebsd }.each do |os|
+%w{ ubuntu debian freebsd centos redhat fedora amazon }.each do |os|
   supports os
 end
 
