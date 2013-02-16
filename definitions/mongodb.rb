9049ddde9fb6017321c1c9700228add78d2cc44d
Michael Wasser <michael.m.wasser@gmail.com>
2013-02-16 02:47:33 -0800

    Update definitions/mongodb.rb
    This is a fix for bug 89 (https://github.com/edelight/chef-mongodb/issues/89) given a breaking change made between chef 10 and 11 -- see https://github.com/edelight/chef-mongodb/issues/89#issuecomment-13646421 for details. This fix will work in both chef 10 and 11.

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -141,7 +141,9 @@ define :mongodb_instance, :mongodb_type => "mongod" , :action => [:enable, :star
   service name do
     supports :status => true, :restart => true
     action service_action
-    notifies service_notifies
+    service_notifies.each do |service_notify|
+      notifies :run, service_notify
+    end
     if !replicaset_name.nil?
       notifies :create, "ruby_block[config_replicaset]"
     end
