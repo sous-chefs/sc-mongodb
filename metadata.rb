48a8b108aaf0c8d84b5a53e484c8febf3f308e91
BK Box <bk@theboxes.org>
2013-05-29 15:51:46 -0500

    That restriction is old and we can use the latest

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "edelight GmbH"
 maintainer_email  "markus.korn@edelight.de"
 license           "Apache 2.0"
 description       "Installs and configures mongodb"
-version           "0.13.0"
+version           "0.13.1"
 
 recipe "mongodb", "Installs and configures a single node mongodb instance"
 recipe "mongodb::10gen_repo", "Adds the 10gen repo to get the latest packages"
@@ -12,7 +12,7 @@ recipe "mongodb::configserver", "Installs and configures a configserver for mong
 recipe "mongodb::shard", "Installs and configures a single shard"
 recipe "mongodb::replicaset", "Installs and configures a mongodb replicaset"
 
-depends "apt", ">= 1.8.2"
+depends "apt"
 depends "yum"
 depends "firewall"
 depends "build-essential"
