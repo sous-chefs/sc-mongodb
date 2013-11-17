16b4846f54ba329e8e63cb4bd67ef2317201f959
Jameson <jamesonjlee@gmail.com>
2013-11-16 23:15:26 -0800

    fix DAEMONUSER instances to be DAEMON_USER always

--- attributes/sysconfig.rb
+++ attributes/sysconfig.rb
@@ -1,8 +1,10 @@
 default['mongodb']['sysconfig']['DAEMON'] = "/usr/bin/$NAME"
-default['mongodb']['sysconfig']['DAEMONUSER'] = node['mongodb']['user']
+default['mongodb']['sysconfig']['DAEMON_USER'] = node['mongodb']['user']
 default['mongodb']['sysconfig']['DAEMON_OPTS'] = "--config #{node['mongodb']['configfile']}"
 default['mongodb']['sysconfig']['CONFIGFILE'] = node['mongodb']['configfile']
-# should mongodb start?
 default['mongodb']['sysconfig']['ENABLE_MONGODB'] = "yes"
+
+# these are backward compat purposes
+default['mongodb']['sysconfig']['DAEMONUSER'] = node['mongodb']['sysconfig']['DAEMON_USER']
 default['mongodb']['sysconfig']['ENABLE_MONGOD'] = node['mongodb']['sysconfig']['ENABLE_MONGODB']
 default['mongodb']['sysconfig']['ENABLE_MONGO'] = node['mongodb']['sysconfig']['ENABLE_MONGODB']
