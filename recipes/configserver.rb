edelight/chef-mongodb

replaces inclusion of mongodb recipe in mongodb::mongos, mongodb::replicaset, mongodb::shard, mongodb::configserver with mongodb::base

recipes/configserver.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::base"
 
 service "mongodb" do
   supports :status => true, :restart => true

recipes/mongos.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::base"
 
 service "mongodb" do
   action [:disable, :stop]

recipes/replicaset.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::base"
 
 # if we are configuring a shard as a replicaset we do nothing in this recipe
 if !node.recipe?("mongodb::shard")

recipes/shard.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb::default"
+include_recipe "mongodb::base"
 
 # disable and stop the default mongodb instance
 service "mongodb" do

sous-chefs/sc-mongodb

recipes/configserver.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::install"
 
 service "mongodb" do
   supports :status => true, :restart => true

recipes/mongos.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::install"
 include_recipe "mongodb::mongo_gem"
 
 service "mongodb" do

recipes/replicaset.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb"
+include_recipe "mongodb::install"
 include_recipe "mongodb::mongo_gem"
 
 # if we are configuring a shard as a replicaset we do nothing in this recipe

recipes/shard.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-include_recipe "mongodb::default"
+include_recipe "mongodb::install"
 
 # disable and stop the default mongodb instance
 service "mongodb" do