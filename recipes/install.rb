b09f7795675ce7203564f1e131ebc676ba8c905d
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-01-18 02:10:16 +0000

    update default replSet selection logic to match mongodb_instance

--- recipes/install.rb
+++ recipes/install.rb
@@ -11,9 +11,14 @@ file node['mongodb']['sysconfig_file'] do
     action :create_if_missing
 end
 
-if node['mongodb']['replicaset_name'].nil? && node.recipe?('mongodb::shard') && node.recipe?('mongodb::replicaset')
-  node.default['mongodb']['config']['replSet'] = "rs_#{node['mongodb']['shard_name']}"
+if node['mongodb']['replicaset_name'].nil? && node.mongodb.is_replicaset
+  if node.mongodb.is_shard
+    node.default['mongodb']['config']['replSet'] = "rs_#{node['mongodb']['shard_name']}"
+  else
+    node.default['mongodb']['config']['replSet'] = 'rs_default'
+  end
 end
+
 
 # just-in-case config file drop
 template node['mongodb']['dbconfig_file'] do
