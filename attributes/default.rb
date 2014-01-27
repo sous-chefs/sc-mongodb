arunjax/cookbooks-1

add mongodb/reload_action attribute to enable config changes without a restart

attributes/default.rb
@@ -36,6 +36,7 @@ default[:mongodb][:root_group] = "root"
 default[:mongodb][:init_dir] = "/etc/init.d"
 
 default[:mongodb][:init_script_template] = "mongodb.init.erb"
+default[:mongodb][:reload_action] = "restart" # or "none"
 
 case node['platform']
 when "freebsd"

sous-chefs/sc-mongodb

attributes/default.rb
@@ -63,6 +63,8 @@ default[:mongodb][:is_replicaset] = nil
 default[:mongodb][:is_shard] = nil
 default[:mongodb][:is_configserver] = nil
 
+default[:mongodb][:reload_action] = "restart" # or "nothing"
+
 case node['platform_family']
 when "freebsd"
   default[:mongodb][:package_name] = "mongo-10gen-server"