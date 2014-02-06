4c5b6c5f447e1b8ea33d180b2080d80e6f5d5b07
Josh Spiewak <jspiewak@brightcove.com>
2014-02-06 15:41:53 -0500

    configservers is what needs to be passed in

--- recipes/mongos.rb
+++ recipes/mongos.rb
@@ -40,10 +40,10 @@ end
 
 mongodb_instance 'mongos' do
   mongodb_type 'mongos'
-  port         node['mongodb']['port']
-  logpath      node['mongodb']['logpath']
-  dbpath       node['mongodb']['dbpath']
-  configserver configsrv
-  enable_rest  node['mongodb']['enable_rest']
-  smallfiles   node['mongodb']['smallfiles']
+  port          node['mongodb']['port']
+  logpath       node['mongodb']['logpath']
+  dbpath        node['mongodb']['dbpath']
+  configservers configsrv
+  enable_rest   node['mongodb']['enable_rest']
+  smallfiles    node['mongodb']['smallfiles']
 end
