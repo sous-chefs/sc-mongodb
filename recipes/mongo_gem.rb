c6d39d38d0f5f026c1e6defbb03c6e72b79ede4d
Jameson <jamesonjlee@gmail.com>
2014-02-05 21:49:14 -0800

    Update mongo_gem.rb

--- recipes/mongo_gem.rb
+++ recipes/mongo_gem.rb
@@ -1,6 +1,6 @@
 # install the mongo and bson_ext ruby gems at compile time to make them globally available
 # TODO: remove bson_ext once mongo gem supports bson >= 2
-gems = %{mongo bson_ext}
+gems = %w{mongo bson_ext}
 
 gems.each do |g|
   if Gem.const_defined?('Version') && Gem::Version.new(Chef::VERSION) < Gem::Version.new('10.12.0')
