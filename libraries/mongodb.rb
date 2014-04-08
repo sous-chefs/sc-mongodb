efc23c688bc1c493cac65018f90d5ece3f81d91d
John Esmet <john.esmet@gmail.com>
2014-04-07 20:41:16 -0400

    Don't bail out if there are no existing members for a replica set in chef-server mode - just configure as a single-member set.

--- libraries/mongodb.rb
+++ libraries/mongodb.rb
@@ -27,13 +27,8 @@ class Chef::ResourceDefinitionList::MongoDB
     require 'rubygems'
     require 'mongo'
 
-    if members.empty?
-      if Chef::Config[:solo]
-        Chef::Log.warn('Cannot search for member nodes with chef-solo, defaulting to single node replica set')
-      else
-        Chef::Log.warn("Cannot configure replicaset '#{name}', no member nodes found")
-        return
-      end
+    if members.empty? && Chef::Config[:solo]
+      Chef::Log.warn('Cannot search for member nodes with chef-solo, defaulting to single node replica set')
     end
 
     begin
