861ac0d96070e014cf472284aee3304ae4431fd8
Jameson <jamesonjlee@gmail.com>
2014-04-17 14:57:34 -0700

    fix mms_agent_spec

--- test/unit/mms_agent_spec.rb
+++ test/unit/mms_agent_spec.rb
@@ -1,41 +1,36 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-describe 'mongodb::mms_agent' do
+describe 'mongodb::mms_monitoring_agent' do
   let(:chef_run) do
-    stub_command("/usr/bin/python -c 'import setuptools'").and_return(true)
     ChefSpec::Runner.new(:platform => 'ubuntu', :version => '12.04') do |n|
-      n.set.mongodb.mms_agent.install_dir = '/usr/local/share'
-      n.set.mongodb.mms_agent.api_key = 'stange key'
+      n.set.mongodb.mms_agent.api_key = 'strange key'
     end
   end
 
-  it 'creates an mmsagent user' do
+  it 'package install the mms_monitoring_agent' do
     chef_run.converge(described_recipe)
-    expect(chef_run).to create_user("#{chef_run.node.mongodb.mms_agent.user}").with(:home => "#{chef_run.node.mongodb.mms_agent.install_dir}")
+    expect(chef_run).to install_package('mongodb-mms-monitoring-agent')
+    expect(chef_run).to render_file('/etc/mongodb-mms/monitoring-agent.config').with_content(/.*=strange key/)
+    resource = chef_run.template('/etc/mongodb-mms/monitoring-agent.config')
+    expect(resource).to notify('service[mongodb-mms-monitoring-agent]').to(:restart).delayed
   end
 
-  it 'installs munin by default' do
-    chef_run.converge(described_recipe)
-    expect(chef_run).to install_package(chef_run.node.mongodb.mms_agent.munin_package)
-  end
+end
 
-  it 'does not install munin if you say so' do
-    chef_run.node.set.mongodb.mms_agent.install_munin = false
-    chef_run.converge(described_recipe)
-    expect(chef_run).not_to install_package(chef_run.node.mongodb.mms_agent.munin_package)
-  end
-
-  describe 'chefspec actually issues real commands on the local client, disabled for now' do
-    xit 'does not clobber anything else' do
-      # add some files to install_dir
-      expected_file = "#{chef_run.node.mongodb.mms_agent.install_dir}/f.txt"
-      expected_string = 'hello mongodb'
-      File.open(expected_file, 'w') { |f| f.write(expected_string) }
-
-      # converge and check the file and string are still there
-      chef_run.converge(described_recipe)
-      File.open(expected_file, 'r') { |f| expect(f.read).to eq(expected_string) }
+describe 'mongodb::mms_backup_agent' do
+  let(:chef_run) do
+    ChefSpec::Runner.new(:platform => 'ubuntu', :version => '12.04') do |n|
+      n.set.mongodb.mms_agent.api_key = 'strange key'
     end
   end
+
+  it 'package install the mms_backup_agent' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to install_package('mongodb-mms-backup-agent')
+    expect(chef_run).to render_file('/etc/mongodb-mms/backup-agent.config').with_content(/.*=strange key/)
+    resource = chef_run.template('/etc/mongodb-mms/backup-agent.config')
+    expect(resource).to notify('service[mongodb-mms-backup-agent]').to(:restart).delayed
+  end
+
 end
