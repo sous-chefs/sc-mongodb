62272958dc96ea8f1fc5406ab33cc92ce09ce715
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-06-26 17:23:29 -0700

    test: replace deprecated should syntax with expect

--- test/unit/install_spec.rb
+++ test/unit/install_spec.rb
@@ -12,26 +12,26 @@ describe 'mongodb::default' do
 
   it 'should include install recipe, and enable mongodb service' do
     chef_run.converge(described_recipe)
-    chef_run.should include_recipe('mongodb::install')
-    chef_run.should enable_service 'mongodb'
+    expect(chef_run).to include_recipe('mongodb::install')
+    expect(chef_run).to enable_service 'mongodb'
   end
 
   it 'package install mongodb-org via 10gen' do
     chef_run.node.set.mongodb.install_method = '10gen'
     chef_run.converge(described_recipe)
-    chef_run.should include_recipe('mongodb::10gen_repo')
-    chef_run.should include_recipe('mongodb::install')
-    chef_run.should install_package 'mongodb-org'
-    chef_run.should enable_service 'mongodb'
+    expect(chef_run).to include_recipe('mongodb::10gen_repo')
+    expect(chef_run).to include_recipe('mongodb::install')
+    expect(chef_run).to install_package 'mongodb-org'
+    expect(chef_run).to enable_service 'mongodb'
   end
 
   it 'package install mongodb-org via mongodb-org' do
     chef_run.node.set.mongodb.install_method = 'mongodb-org'
     chef_run.converge(described_recipe)
-    chef_run.should include_recipe('mongodb::10gen_repo')
-    chef_run.should include_recipe('mongodb::install')
-    chef_run.should install_package 'mongodb-org'
-    chef_run.should enable_service 'mongodb'
+    expect(chef_run).to include_recipe('mongodb::10gen_repo')
+    expect(chef_run).to include_recipe('mongodb::install')
+    expect(chef_run).to install_package 'mongodb-org'
+    expect(chef_run).to enable_service 'mongodb'
   end
 
 end
