b4007cb1a8f26053f2cf2f485573222e4ebcb927
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-06-26 19:16:10 -0700

    test/unit: seperate ReplicasetMember spec from configure_replicaset()

--- test/unit/configure_replicaset_spec.rb
+++ test/unit/configure_replicaset_spec.rb
@@ -9,63 +9,6 @@ describe 'configure_replicaset' do
   end
 end
 
-describe 'ReplicasetMember' do
-  let(:raw) do
-    {
-      'fqdn' => 'a.b.c',
-      'ipaddress' => '1.2.3.4',
-      'mongodb' => {
-        'config' => {
-          'port' => 27_017
-        },
-        'replica_arbiter_only' => 'true',
-        'replica_slave_delay' => 5,
-        'replica_tags' => {},
-        'replica_votes' => 1,
-        'replica_build_indexes' => true,
-        'replica_hidden' => true
-      }
-    }
-  end
-  it 'should convert to hash' do
-    member = Chef::ResourceDefinitionList::MongoDB::ReplicasetMember.new raw
-    expected = {
-      'host' => 'a.b.c:27017',
-      'arbiterOnly' => true,
-      'buildIndexes' => true,
-      'hidden' => true,
-      'slaveDelay' => 5,
-      'priority' => 0,
-      'tags' => {},
-      'votes' => 1
-    }
-    expect(member.to_h).to eq(expected)
-  end
-  it 'should convert to hash with id when given' do
-    member = Chef::ResourceDefinitionList::MongoDB::ReplicasetMember.new raw, 5
-    expected = {
-      'host' => 'a.b.c:27017',
-      'arbiterOnly' => true,
-      'buildIndexes' => true,
-      'hidden' => true,
-      'slaveDelay' => 5,
-      'priority' => 0,
-      'tags' => {},
-      'votes' => 1,
-      '_id' => 5,
-    }
-    expect(member.to_h).to eq(expected)
-  end
-  it 'should convert to hash with id when given' do
-    member = Chef::ResourceDefinitionList::MongoDB::ReplicasetMember.new raw, 5
-    expected = {
-      'host' => '1.2.3.4:27017',
-      '_id' => 5,
-    }
-    expect(member.to_h_with_ipaddress).to eq(expected)
-  end
-end
-
 describe 'ReplicasetConfig' do
   let(:member) do
     Chef::ResourceDefinitionList::MongoDB::ReplicasetMember.new(
