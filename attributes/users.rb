04f3da87f7830f379df23657072a0bcad1828ce5
Colin Hubert <chubert@turbine.com>
2014-07-10 10:34:26 -0400

    fixing the auth attribute defaulting to true for environments that don't include the user_management recipe

--- attributes/users.rb
+++ attributes/users.rb
@@ -1,4 +1,6 @@
-default['mongodb']['config']['auth'] = true
+# Set this to true in your chef json
+# to require all users to authenticate
+default['mongodb']['config']['auth'] = false
 
 default['mongodb']['admin'] = {
   'username' => 'admin',
