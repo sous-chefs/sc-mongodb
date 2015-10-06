5812efa75de8d06ae6652e725a7ad2de38335838
Jean-Marc Collin <jm.collin.78@gmail.com>
2015-10-06 22:39:26 +0200

    Update replicaset.rb

--- recipes/replicaset.rb
+++ recipes/replicaset.rb
@@ -26,7 +26,6 @@ ruby_block 'chef_gem_at_converge_time' do
   block do
     node['mongodb']['ruby_gems'].each do |gem, version|
       version = Gem::Dependency.new(gem, version)
-        Chef::Log.info("jmcnet/mongodb-coookbook : updated version is #{version}")
       Chef::Provider::Package::Rubygems::GemEnvironment.new.install(version)
     end
   end
