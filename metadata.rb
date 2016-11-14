d10dfcd8f84015b2b6c0c814a58466dffa446ab6
Grant Ridder <grant@teambanjo.com>
2016-11-14 10:40:07 -0800

    Add recipe's back to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,15 @@ description       'Installs and configures mongodb'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.16.3'
 
+recipe 'mongodb', 'Installs and configures a single node mongodb instance'
+recipe 'mongodb::10gen_repo', 'Adds the 10gen repo to get the latest packages'
+recipe 'mongodb::mongos', 'Installs and configures a mongos which can be used in a sharded setup'
+recipe 'mongodb::configserver', 'Installs and configures a configserver for mongodb sharding'
+recipe 'mongodb::shard', 'Installs and configures a single shard'
+recipe 'mongodb::replicaset', 'Installs and configures a mongodb replicaset'
+recipe 'mongodb::mms_monitoring_agent', 'Installs and configures a MongoDB MMS Monitoring Agent'
+recipe 'mongodb::mms_backup_agent', 'Installs and configures a MongoDB MMS Backup Agent'
+
 depends 'apt', '>= 1.8.2'
 depends 'yum', '>= 3.0'
 depends 'python'
