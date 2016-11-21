b32102b9b08c087f4a2b78715249b04f80e5f4f3
Grant Ridder <grant@teambanjo.com>
2016-11-20 21:28:25 -0800

    Remove Chef 10 support

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,15 +24,8 @@ include_recipe 'sc-mongodb::install'
 # allow mongodb_instance to run if recipe isn't included
 allow_mongodb_instance_run = true
 conflicting_recipes = %w(sc-mongodb::replicaset sc-mongodb::shard sc-mongodb::configserver sc-mongodb::mongos sc-mongodb::mms_agent)
-chef_major_version = Chef::VERSION.split('.').first.to_i
-if chef_major_version < 11
-  conflicting_recipes.each do |recipe|
-    allow_mongodb_instance_run &&= false if node.recipe?(recipe)
-  end
-else
-  conflicting_recipes.each do |recipe|
-    allow_mongodb_instance_run &&= false if node.run_context.loaded_recipe?(recipe)
-  end
+conflicting_recipes.each do |recipe|
+  allow_mongodb_instance_run &&= false if node.run_context.loaded_recipe?(recipe)
 end
 
 mongodb_instance node['mongodb']['instance_name'] do
