6ab1900a26192657fb7b653a933a35b722224ccc
Carlos Mafla <gigo6000@hotmail.com>
2017-03-06 13:25:46 -0500

    Fix indentation

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -197,12 +197,12 @@ define :mongodb_instance,
   service new_resource.name do
     case node['platform']
     when 'ubuntu'
-        if node['platform_version'] == '16.04'
-          provider Chef::Provider::Service::Systemd
-        end  
-    else 
-        provider Chef::Provider::Service::Upstart if node['mongodb']['apt_repo'] == 'ubuntu-upstart'
-    end 
+      if node['platform_version'] == '16.04'
+        provider Chef::Provider::Service::Systemd
+      end
+    else
+      provider Chef::Provider::Service::Upstart if node['mongodb']['apt_repo'] == 'ubuntu-upstart'
+    end
     supports status: true, restart: true
     action new_resource.service_action
     new_resource.service_notifies.each do |service_notify|
