33d89b351d46286aae583511337eb51ad9ac3d21
Yorgos Saslis <g.saslis@gmail.com>
2017-03-08 11:46:52 +0200

    Introduce min version dependency on build-essential
    avoid issues with older versions of the build-essential cookbook, resulting in: 
    
    `No resource or method named `build_essential' for `Chef::Recipe "install"'`
    
    Fixes #134

--- metadata.rb
+++ metadata.rb
@@ -18,7 +18,7 @@ recipe 'sc-mongodb::mms_backup_agent', 'Installs and configures a MongoDB MMS Ba
 depends 'apt', '>= 1.8.2'
 depends 'yum', '>= 3.0'
 depends 'python'
-depends 'build-essential'
+depends 'build-essential', '>= 5.0.0'
 
 %w(ubuntu debian centos redhat amazon).each do |os|
   supports os
