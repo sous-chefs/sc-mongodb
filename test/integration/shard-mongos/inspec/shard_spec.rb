0db67fa97f921e77b9b44b77b2c9b8258c0a8729
Grant Ridder <shortdudey123@gmail.com>
2017-05-18 19:01:19 -0700

    Fix Style/RegexpLiteral offense

--- test/integration/shard-mongos/inspec/shard_spec.rb
+++ test/integration/shard-mongos/inspec/shard_spec.rb
@@ -16,7 +16,7 @@ describe bash('mongo --eval "sh.status()"') do
   its('exit_status') { should eq 0 }
 
   # Sharding
-  its(:stdout) { should match /.*^\s{2}shards:\n\t{  "_id" : "kitchen1",  "host" : "kitchen1\/mongo1:27017,mongo2:27017,mongo3:27017" }$.*/ }
+  its(:stdout) { should match %r(.*^\s{2}shards:\n\t{  "_id" : "kitchen1",  "host" : "kitchen1/mongo1:27017,mongo2:27017,mongo3:27017" }$.*) }
 
   # Sharded collection
   its(:stdout) { should match /.*^\t{  "_id" : "test",  "primary" : "kitchen1",  "partitioned" : true }\n\t{2}test.testing\n\t{3}shard key: { "_id" : 1 }$.*/ }
