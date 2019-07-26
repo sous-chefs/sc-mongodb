b05c84bf098a9f85d48357c0e27c8f037f16b4fc
Tim Smith <tsmith@chef.io>
2019-07-26 00:58:18 -0700

    Use chef-style comment header & modes as strings (#230)
    Auto-corrected with Cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: sc-mongodb
+# Cookbook:: sc-mongodb
 # Attributes:: default
 #
-# Copyright 2010, edelight GmbH
+# Copyright:: 2010, edelight GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
