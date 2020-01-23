af755e77ee032b55b88a1751a981484fe8a7b236
Tim Smith <tsmith84@gmail.com>
2020-01-22 16:26:47 -0800

    Remove deprecated long_description metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures mongodb'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/sous-chefs/mongodb' if respond_to?(:source_url)
 issues_url        'https://github.com/sous-chefs/mongodb/issues' if respond_to?(:issues_url)
 chef_version      '>= 13.0'
