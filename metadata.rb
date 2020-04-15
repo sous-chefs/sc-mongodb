2a602ec3dfc042075d88914247c738a03906fd00
Jason Field <4923914+Xorima@users.noreply.github.com>
2020-04-15 19:50:01 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license           'Apache-2.0'
 description       'Installs and configures mongodb'
 source_url        'https://github.com/sous-chefs/mongodb'
 issues_url        'https://github.com/sous-chefs/mongodb/issues'
-chef_version      '>= 13.0'
+chef_version      '>= 14.0'
 version           '3.0.0'
 
 supports 'amazon'
